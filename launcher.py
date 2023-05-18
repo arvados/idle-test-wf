f3dba69a194048e35bad155002403f28ffb14c8a
Peter Amstutz <peter.amstutz@curii.com>
2023-05-18 16:42:11 -0400

    fix typo
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- launcher.py
+++ launcher.py
@@ -5,7 +5,7 @@ tickers = int(sys.argv[1])
 
 print("launching %s tickers" % tickers)
 
-for i in range(1, ticks+1):
+for i in range(1, tickers+1):
     subprocess.run(["arvados-cwl-runner", "--no-wait", "--name=tick-tock-test-%s" % i, "tordo-7fd4e-2bz5gaoev4dnjn6", "--ticksPerStep=3"])
 
 print("done")
