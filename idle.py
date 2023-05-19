f7b2e31edf5c31534ff0c76f42d3686a67fef1e8
Peter Amstutz <peter.amstutz@curii.com>
2023-05-19 15:47:39 -0400

    Flush stdout
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- idle.py
+++ idle.py
@@ -3,10 +3,10 @@ import sys
 
 ticks = int(sys.argv[1])
 
-print("ticking %s times" % ticks)
+print("ticking %s times" % ticks, flush=True)
 
 for i in range(1, ticks+1):
     time.sleep(10)
-    print("tick %s / %s" % (i, ticks))
+    print("tick %s / %s" % (i, ticks), flush=True)
 
-print("done")
+print("done", flush=True)
