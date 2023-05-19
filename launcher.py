2af9fe13c88d2286dea33454061300382849e763
Peter Amstutz <peter.amstutz@curii.com>
2023-05-19 19:27:14 -0400

    wait after submitting everything
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- launcher.py
+++ launcher.py
@@ -1,11 +1,19 @@
 import sys
 import subprocess
+import time
 
 tickers = int(sys.argv[1])
 
-print("launching %s tickers" % tickers)
+print("launching %s tickers" % tickers, flush=True)
 
 for i in range(1, tickers+1):
-    subprocess.run(["arvados-cwl-runner", "--no-wait", "--name=tick-tock-test-%s" % i, "tordo-7fd4e-2bz5gaoev4dnjn6", "--ticksPerStep=3"])
+    subprocess.run(["arvados-cwl-runner", "--no-wait", "--name=tick-tock-test-%s" % i, "tordo-7fd4e-2bz5gaoev4dnjn6", "--ticksPerStep=90"])
 
-print("done")
+ticks=90*5
+print("ticking", flush=True)
+
+for i in range(1, ticks+1):
+    time.sleep(10)
+    print("tick %s / %s" % (i, ticks), flush=True)
+
+print("done", flush=True)
